6a92f2742a17e7118fd2a87f45d15f7bf667cc8d
CoolL <alfastar650@gmail.com>
2026-07-09 19:08:56 +0300

    global css, remove keepalive(fixed scrollbar jumping)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    keepalive: true
-  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts"]
+  modules: ["@nuxt/fonts"],
+  css: [
+    '~~/assets/css/style.css'
+  ]
 })
